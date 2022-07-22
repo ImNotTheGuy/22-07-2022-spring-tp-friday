3d46f0a7daf6938fb0f871e9ab7372105b02eda0
Ludo <lraulin843@gmail.com>
2022-07-22 10:47:09 +0200

    Added controllers + solved some issues on services

---
+++ src/main/java/com/example/friday/api/AnswerController.java
@@ -0,0 +1,45 @@
+package com.example.friday.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.friday.business.AnswerService;
+import com.example.friday.entity.Answer;
+
+@RestController
+public class AnswerController {
+
+    @Autowired
+    AnswerService answerService;
+
+    @PostMapping("answer")
+    public void create(@RequestBody Answer answer) {
+        answerService.create(answer);
+    }
+
+    @GetMapping("answer")
+    public List<Answer> findAll(){
+        return answerService.findAll();
+    }
+
+    @GetMapping("answer/{id}")
+    public Answer findById(@PathVariable("id") long id){
+        return answerService.findById(id);
+    }
+
+    @PatchMapping("answer/{id}")
+    public void update(@RequestBody Answer answer, @PathVariable("id") long id){
+        Answer currentAnswer = findById(id);
+        currentAnswer.setAnswer(answer.getAnswer());
+        currentAnswer.setCorrect(answer.isCorrect());
+        answerService.update(currentAnswer);
+    }
+
+}
---
+++ src/main/java/com/example/friday/api/QuestionController.java
@@ -0,0 +1,45 @@
+package com.example.friday.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.friday.business.QuestionService;
+import com.example.friday.entity.Question;
+
+@RestController
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @PostMapping("question")
+    public void create(@RequestBody Question question) {
+        questionService.create(question);
+    }
+
+    @GetMapping("question")
+    public List<Question> findAll(){
+        return questionService.findAll();
+    }
+
+    @GetMapping("question/{id}")
+    public Question findById(@PathVariable("id") long id){
+        return questionService.findById(id);
+    }
+
+    @PatchMapping("question/{id}")
+    public void update(@RequestBody Question question, @PathVariable("id") long id){
+        Question currentQuestion = findById(id);
+        currentQuestion.setQuestion(question.getQuestion());
+        currentQuestion.setAnswer(question.getAnswer());
+        questionService.update(currentQuestion);
+    }
+
+}
