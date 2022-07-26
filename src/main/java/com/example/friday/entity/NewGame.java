97b6e716bfe1a3fdf2c4d5793cca382dbdc0b296
Ludo <lraulin843@gmail.com>
2022-07-26 09:13:46 +0200

    removed unused libraries

--- src/main/java/com/example/friday/entity/NewGame.java
+++ src/main/java/com/example/friday/entity/NewGame.java
@@ -1,6 +1,5 @@
 package com.example.friday.entity;
 
-import java.util.List;
 import java.util.Map;
 
 public class NewGame {
--- src/main/java/com/example/friday/entity/Player.java
+++ src/main/java/com/example/friday/entity/Player.java
@@ -1,6 +1,5 @@
 package com.example.friday.entity;
 
-import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
